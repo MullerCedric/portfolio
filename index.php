30eada22a8e21acfb84527865106e27cdf666c5a
MullerCedric <muller.cedric.96@gmail.com>
2018-08-21 19:05:20 +0200

    Update index file as a basic template file

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
+<?php /*
+  * Basic page
+  * Basic template used for the wordpress user pages such as the About page
+  */ ?>
 <!DOCTYPE html>
 <html lang="fr">
-<?php
-get_header(); ?>
+<?php get_header(); ?>
 <body>
 <header>
     <h1>
 		<?php the_title(); ?>
     </h1>
 </header>
-<main id="content">
-
+<main id="content"><?php
+	if ( have_posts() ): while ( have_posts() ): the_post();
+        if ( have_rows( 'sections' ) ): while ( have_rows( 'sections' ) ): the_row(); ?>
+            <section>
+                <h2><?= get_sub_field( 'title' ); ?></h2>
+                <?= get_sub_field('text'); ?>
+            </section><?php
+        endwhile; endif; ?>
+        <div><?php if ( have_rows( 'cta' ) ): while ( have_rows( 'cta' ) ): the_row(); ?>
+            <a href="<?= get_sub_field('url'); ?>"><?= get_sub_field('label'); ?></a><?php
+		endwhile; endif; ?>
+        </div><?php
+    endwhile; endif;?>
 </main><?php
 get_template_part( 'inc/part', 'nav' );
 get_footer(); ?>
