1e91a94b54774bf3caa4b9ae33bbe8ab99dc8d81
MullerCedric <muller.cedric.96@gmail.com>
2018-08-21 13:18:23 +0200

    Create archive page HTML

---
+++ archive-project.php
@@ -0,0 +1,36 @@
+<?php /*
+  * Archive page
+  * Displays all the projects
+  */ ?>
+<!DOCTYPE html>
+<html lang="fr">
+<?php get_header(); ?>
+<body>
+<header>
+	<h1>
+		<?= get_bloginfo('name'); ?>
+	</h1>
+</header>
+<main id="content"><?php
+	$projects = new WP_Query(['post_type' => 'project']);
+	if($projects->have_posts()): while($projects->have_posts()): $projects->the_post();
+	$content = get_field('basic_info'); ?>
+        <article class="c-archProject">
+            <div class="c-archProject__thumbnail">
+                <?php if($content['thumb']['url']) echo '<img src="' . $content['thumb']['url'] . '" alt ="Miniature du projet ' . get_the_title() . '">"'; ?>
+            </div>
+            <h2 class="c-archProject__title"><?php the_title(); ?></h2>
+            <div class="c-archProject__content">
+                <?= $content['desc'] ?>
+            </div>
+            <div>
+                <a href="<?php the_permalink(); ?>" title="Voir le projet <?= get_the_title(); ?>" class="c-archProject__permalink">Voir le projet</a>
+            </div>
+        </article>
+	<?php endwhile; endif; ?>
+</main><?php
+get_template_part( 'inc/part', 'nav' );
+get_footer(); ?>
+
+</body>
+</html>
--- core/Post_types/project.php
+++ core/Post_types/project.php
@@ -6,14 +6,17 @@ function cm_register_project() {
                 'name' => __( 'Projects' ),
                 'singular_name' => __( 'Project' ),
                 'add_new_item' => 'Add new project',
-	            'edit_item' => 'Update project'
+	            'edit_item' => 'Update project',
+	            'all_items' => 'All projects'
             ],
             'description' => 'All of your projects',
             'public' => true,
             'menu_position' => 20,
             'menu_icon' => 'dashicons-format-aside',
             'supports' => ['title'],
-            'rewrite' => ['slug' => 'projects'],
+            'taxonomies'  => array( 'category' ),
+            'has_archive' => true,
+            'rewrite' => ['slug' => 'projets'],
         ]
     );
 }
